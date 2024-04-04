ffbb987324adae4410d1d294ab02287c529318e7
cnnmon <cnnmonn@gmail.com>
2024-04-04 00:10:55 -0700

    react webview works!!
    built a react + typescript + tailwind project & used parcel to build

--- src/chat/ChatPanel.ts
+++ src/chat/ChatPanel.ts
@@ -14,8 +14,8 @@ export class ChatPanel implements vscode.WebviewViewProvider {
   }
 
   private _getHtmlForWebview(webview: vscode.Webview) {
-    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-    const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "web", "dist", "index.js"));
+    const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "web", "dist", "index.css"));
 
     // Use a nonce to whitelist which scripts can be run
     const nonce = getNonce();
@@ -23,21 +23,15 @@ export class ChatPanel implements vscode.WebviewViewProvider {
     return `<!DOCTYPE html>
     <html lang="en">
       <head>
-        <meta charset="UTF-8">
-        <!--
-          Use a content security policy to only allow loading images from https or from our extension directory,
-          and only allow scripts that have a specific nonce.
-        -->
-        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="${styleUri}" rel="stylesheet">
+        <link rel="stylesheet" href="${styleUri}" />
       </head>
       <body>
-        <h1>Chat</h1>
-        <div id="chat"></div>
-        <script nonce="${nonce}" src="${scriptUri}"></script>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root"></div>
+        <script src="${scriptUri}"></script>
       </body>
-    </html>`;
+    </html>
+    `;
   }
 }
 
---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function App() {
+    return (<div className="bg-gradient-to-r from-blue-600 to-purple-500 p-10">
+      <p className="text-white/80 text-xl font-semibold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, explicabo
+        doloremque deserunt, voluptates, fugiat dolorem consectetur odio autem
+        quas ipsa veniam ducimus necessitatibus exercitationem numquam assumenda
+        natus beatae sed velit!
+      </p>
+    </div>);
+}
+exports.default = App;
+//# sourceMappingURL=App.js.map
