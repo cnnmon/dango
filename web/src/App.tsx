f3a39ddfb25c7f5ce6401fe6435a99781e65d098
cnnmon <cnnmonn@gmail.com>
2024-04-09 17:04:29 -0700

    use bash to update react webview appropriately

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className="bg-gradient-to-r from-blue-600 to-purple-500 p-10">
-      <textarea className="bg-white p-2 rounded-md w-full h-40" placeholder="Type something..."></textarea>
-      <button className="bg-blue-500 text-white p-2 rounded-md mt-2">Submit</button>
+      <textarea className="bg-white p-2 rounded-md w-full h-40" placeholder="FUCK something..."></textarea>
+      <button className="bg-blue-500 text-white p-2 rounded-md mt-2">asdjaskdsakda</button>
     </div>
   );
 }
