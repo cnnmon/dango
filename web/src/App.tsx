78f398551fd56c9a0be270539918e0dadedd13d8
cnnmon <cnnmonn@gmail.com>
2024-05-10 10:59:09 -0700

    save last step's messages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -42,21 +42,36 @@ async function generateStepsAndUpdateDesignDoc() {
 
 /* MAIN APP */
 
-export default function App() {
-  /* LOCAL STORAGE */
-  const initialStep = parseInt(localStorage.getItem("currentStep") || "0");
+const defaultMessages = [
+  botSays(`Hi! I'm Dango, your project co-collaborator. I work off of your design doc to generate code and improve your workflow. Let's get started!`),
+  botSays(`No design doc found. Reply with '${EXECUTE_PHRASE}' to generate a starter template. Or, create design.md with information on your intended project in the root of your workspace.`),
+]
 
+function getSavedMessages(fallbackMessages: Message[]) {
+  return {
+    stepIdx: parseInt(localStorage.getItem("savedStepIdx") || "0"),
+    messages: JSON.parse(localStorage.getItem("savedStepMessages") || JSON.stringify(fallbackMessages))
+  }
+}
+
+function resetSavedMessages() {
+  localStorage.removeItem("savedStepIdx");
+  localStorage.removeItem("savedStepMessages");
+}
+
+function saveMessages(stepIdx: number, messages: Message[]) {
+  localStorage.setItem("savedStepIdx", stepIdx.toString());
+  localStorage.setItem("savedStepMessages", JSON.stringify(messages));
+}
+
+export default function App() {
   /* STATES */
   const [designDoc, setDesignDoc] = useState<string | null>(null);
   const [allFileContents, setAllFileContents] = useState<{ name: string; content: string }[]>([]);
   const [steps, setSteps] = useState<Step[]>([]);
-  const [messages, setMessages] = useState<Message[]>([
-      botSays(`Hi! I'm Dango, your project co-collaborator. I work off of your design doc to generate code and improve your workflow. Let's get started!`),
-      botSays(`No design doc found. Reply with '${EXECUTE_PHRASE}' to generate a starter template. Or, create design.md with information on your intended project in the root of your workspace.`),
-    ]
-  );
+  const [currentStepIdx, setCurrentStepIdx] = useState(0);
+  const [messages, setMessages] = useState<Message[]>([]);
   const [isDangoLoading, setIsDangoLoading] = useState(false);
-  const [currentStepIdx, setCurrentStepIdx] = useState(initialStep);
   const [textareaValue, setTextareaValue] = useState<string>("");
   const messagesEndRef = useRef<null | HTMLDivElement>(null)
 
@@ -83,23 +98,34 @@ export default function App() {
       setMessages([botSays(`Design doc found, but unable to read steps. Reply with '${EXECUTE_PHRASE}' and I'll generate steps for us to work on. (This might take a minute or two.)`)]);
       return;
     }
+    setSteps(foundSteps);
+
+    // Get saved step index and messages
+    const { stepIdx, messages: savedMessages } = getSavedMessages(getDesignDocConfirmation(foundSteps[currentStepIdx]));
 
     // Reset step index if it exceeds the number of steps
-    let stepIdx = currentStepIdx;
-    if (currentStepIdx >= foundSteps.length) {
-      stepIdx = 0;
-      setCurrentStepIdx(stepIdx);
-      window.localStorage.setItem("currentStep", stepIdx.toString());
+    if (stepIdx >= foundSteps.length) {
+      resetSavedMessages();
+      setCurrentStepIdx(0);
+      setMessages(getDesignDocConfirmation(foundSteps[stepIdx]));
     }
 
-    setSteps(foundSteps);
-    setMessages(getDesignDocConfirmation(foundSteps[stepIdx]));
+    setCurrentStepIdx(stepIdx);
+    setMessages(savedMessages);
   }
 
   const handleStepChange = (newStep: number) => {
     setCurrentStepIdx(newStep);
-    window.localStorage.setItem("currentStep", newStep.toString());
-    setMessages(getDesignDocConfirmation(steps[newStep]));
+
+    // Check if we're navigating to the "currentStepIdx"; if so, load messages from local storage
+    const fallbackMessages = getDesignDocConfirmation(steps[newStep]);
+    const { stepIdx, messages } = getSavedMessages(fallbackMessages);
+    if (newStep === stepIdx) {
+      setMessages(messages);
+      return;
+    }
+
+    setMessages(fallbackMessages);
   }
 
   const handleUserMessage = async () => {
@@ -133,9 +159,15 @@ export default function App() {
 
       // If new messages are generated, that means we can instantly stop the loading state
       // Else, we need to wait for vscode to respond
+      const allMessages = [...messages, ...newMessages];
       if (response.newMessages.length) {
-        addMessages(newMessages);
+        setMessages(allMessages);
         setIsDangoLoading(false);
+      }
+
+      // Only save current step to come back to if you send a message in it
+      if (steps) {
+        saveMessages(currentStepIdx, allMessages);
       }
     });
   }
@@ -146,6 +178,7 @@ export default function App() {
       const message = event.data;
       const { type, value } = message;
       console.log("Received message:", message);
+
       switch (type) {
         case "addFile":
           console.log("Received file", value);
